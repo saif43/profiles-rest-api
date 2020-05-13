c1c5c20f20cd9095f6076b009982fbca1363d23a
saif43 <saif.ahmed.anik.0@gmail.com>
2020-05-14 05:04:17 +0600

    8.9

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,8 +1,11 @@
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from profiles_api import serializers
 
 class HelloAPIView(APIView):
     """Test API View"""
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
         """Returns a dictionary"""
@@ -10,3 +13,15 @@ class HelloAPIView(APIView):
         an_apiview = ['1','2','3','4']
 
         return Response({'message': 'Hello', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        """Create a hello message with our name"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = 'Hello {}'.format(name)
+
+            return Response({'message': message})
+        
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
