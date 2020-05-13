8b4c706ce8c5ab114308c02b4882f8767d7c3bdc
saif43 <saif.ahmed.anik.0@gmail.com>
2020-05-14 04:01:04 +0600

    Added API View

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class HelloAPIView(APIView):
+    """Test API View"""
+
+    def get(self, request, format=None):
+        """Returns a dictionary"""
+
+        an_apiview = ['1','2','3','4']
+
+        return Response({'message': 'Hello', 'an_apiview': an_apiview})
