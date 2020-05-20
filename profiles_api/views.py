aec23eda9a4acb588a42bd666c1e7c12ebe58965
saif43 <saif.ahmed.anik.0@gmail.com>
2020-05-20 20:50:01 +0600

    9.6

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -31,6 +31,8 @@ class HelloAPIView(APIView):
 class HelloViewSet(viewsets.ViewSet):
     """Test API ViewSet"""
 
+    serializer_class = serializers.HelloSerializer
+
     def list(self, request):
         """Return a Hello message"""
 
@@ -42,3 +44,21 @@ class HelloViewSet(viewsets.ViewSet):
                 'a_viewset': a_viewset
             })
         )
+
+    def create(self, request):
+        """Create a hello message"""
+
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = 'Hello {}'.format(name)
+        
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status = status.HTTP_400_BAD_REQUEST
+            )
+
+    
